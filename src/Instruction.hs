d0f12ae65e35dad3f6a796f734a5b2ce81e4a769
Brett Schreiber <schreiberbrett@gmail.com>
2019-02-20 17:39:13 -0500

    Add files via upload

---
+++ src/Instruction.hs
@@ -0,0 +1,46 @@
+module Instruction (parseInstruction, Instruction (Define, Label, Call), Argument (Referenced, Lambda, Immediate), Identifier) where
+
+import Data.Char (isDigit)
+import Symbol (Symbol (Atom, Symbols), isAtom)
+
+data Instruction = Define Identifier Argument | Label Identifier | Call Identifier [Argument]
+data Argument = Referenced Identifier | Lambda [Identifier] [Instruction] | Immediate Int
+type Identifier = String
+
+instance Show Instruction where
+    show instruction = "(" ++ toString instruction ++ ")" where
+        toString (Define id arg) = "define " ++ id ++ " " ++ show arg
+        toString (Label id) = "label " ++ id
+        toString (Call id []) = id
+        toString (Call id args) = id ++ " " ++ (unwords . map show) args
+
+instance Show Argument where
+    show (Referenced id) = id
+    show (Lambda params instructions) =
+        "(lambda " ++ unwords params ++ "\n\t" ++
+        concatMap show instructions ++ ")"
+    show (Immediate int) = show int
+
+parseInstruction :: Symbol -> Instruction
+parseInstruction (Symbols [Atom "define", id, arg]) =
+    Define (parseIdentifier id) (parseArgument arg)
+parseInstruction (Symbols [Atom "label", id]) = Label (parseIdentifier id)
+parseInstruction (Symbols (id:args)) = Call (parseIdentifier id) (map parseArgument args)
+
+parseArgument :: Symbol -> Argument
+parseArgument (Atom a) =
+    if ((\c -> isDigit c || c == '-') . head) a
+    then Immediate $ read a
+    else Referenced a
+
+parseArgument (Symbols (Atom "lambda" : idsAndInstructions)) = 
+    Lambda (map parseIdentifier ids) (map parseInstruction instructions) where
+    (ids, instructions) = span isAtom idsAndInstructions
+
+parseArgument (Symbols (Atom "λ" : idsAndInstructions)) =
+    Lambda (map parseIdentifier ids) (map parseInstruction instructions) where
+    (ids, instructions) = span isAtom idsAndInstructions
+
+parseIdentifier :: Symbol -> Identifier
+parseIdentifier (Atom a) = a
+parseIdentifier symbols = error $ "Expected identifier but got: " ++ show symbols
---
+++ src/RISCV.hs
@@ -0,0 +1,34 @@
+module RISCV (registers, primitiveInstructions) where
+
+import Data.Set (Set, fromList)
+
+registers :: Set String
+registers = fromList $
+    allOf 'x' [0..31] ++
+    allOf 's' [0..11] ++
+    allOf 'a' [0..7] ++
+    allOf 't' [0..6] ++
+        ["ra", "sp", "gp", "tp", "fp", "zero"]
+        where allOf char range = [char : show i | i <- range]
+
+
+primitiveInstructions :: Set String
+primitiveInstructions = fromList [
+        "lui",
+        "auipc",
+        "jal", "jalr",
+        "beq", "bne", "blt", "bge", "bltu", "bgeu",
+        "lb", "lh", "lw", "lbu", "lhu",
+        "sb", "sh", "sw",
+        "addi",
+        "slti", "sltiu",
+        "xori", "ori", "andi",
+        "slli", "srli", "srai",
+        "add", "sub", "sll",
+        "slt", "sltu",
+        "xor", "or", "and",
+        "srl", "sra",
+        "fence", "fence.i",
+        "ecall", "ebreak",
+        "csrrw", "csrrs", "csrrc",
+        "csrrwi", "csrrsi", "csrrci"]
